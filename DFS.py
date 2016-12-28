e907e1086595df69754b081c56837063414f4dc0
JerryWang304 <wang_feng_bro@163.com>
2016-12-28 22:37:32 +0800

    add dsf to divide data into some groups

---
+++ DFS.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+# 返回连通分量的个数
+# G: np.ndarray
+import numpy as np
+# 从s出发dfs
+visited = [0 for i in range(2000)]
+def dfs_once(G,s,visited_nodes):
+    n = G.shape[0] # 顶点个数
+    assert G.shape[0] == G.shape[1] and type(s) == int and s >=0 and s < n
+    visited[s] = 1 # 当前节点访问结束
+    visited_nodes.append(s)
+    #print s
+    for i in range(n):
+        if G[i][s] == 1 and visited[i] == 0:
+            dfs_once(G,i,visited_nodes)
+
+def dfs(G):
+    #count = 0
+    n = G.shape[0] # 顶点个数
+    connected_component = []
+    assert G.shape[0] == G.shape[1]
+    for i in range(n):
+        if visited[i] == 0:
+            visited_nodes = []
+            dfs_once(G,i,visited_nodes)
+            #count += 1
+            connected_component.append(visited_nodes)
+            #print visited_nodes
+    return connected_component
+if __name__ == '__main__':
+    G = np.array([[1,1,1,0],[1,1,0,0],[1,0,1,0],[0,0,0,1]])
+    #dfs_once(G,0)
+    num,cc = dfs(G)
+    print num
+
+    
+    print cc
--- fuzzy_decision_table.py
+++ fuzzy_decision_table.py
@@ -6,7 +6,8 @@
 # 时间: 2016-12-8
 import numpy as np
 from  sklearn.datasets import load_iris
-
+from math import sqrt
+from DFS import dfs
 class FuzzyDecisionTable(object):
     # table: input data set
     # table type: numpy.ndarray
@@ -14,33 +15,71 @@ class FuzzyDecisionTable(object):
         self.table = table
     
     # 模糊相似关系
-    # input: two real numbers
+    # input: two real vectors
+    # x,y: ndarry
     def f(self,x,y):
-        if abs(x-y)<0.1:
-            return 1
-        else:
-            return 0
-    # 选择一列数=数据，用f计算出一个模糊相似矩阵
-    # col: 某一列，类型为integer
-    # return type: np.ndarry
-    def fuzzy_matrix(self,col):
+        assert x.shape[0] == y.shape[0]
+        nums  = x.shape[0] # 包含元素个数
+        print "x = ",x
+        print "y = ",y
+        print x*y
+        print "sum = ",sum(x*y)
+        numerator = sum(x*y)
+        sum_x = 0 # x 平方和
+        sum_y = 0 # y 平方和 
+        for i in range(0,nums):
+            
+            sum_x += x[i]**2
+            sum_y += y[i]**2
+        length_of_x = sqrt(sum_x)
+        length_of_y = sqrt(sum_y)
+        denomimator = length_of_y*length_of_x
+        return numerator/denomimator
+
+    # 用f计算出一个模糊相似矩阵
+    def get_fuzzy_matrix(self):
         (rows,cols) = self.table.shape
-        assert type(col) == int and col >=0 and col < cols
-        data = self.table[:,col] # 第col列数据取出来
-        # 两两计算f，存到一个矩阵中，矩阵大小为rows*rows
-        f_matrix = np.identity(rows)
+        fuzzy_matrix = np.identity(rows)
+        # 向量两两计算f
         for i in range(0,rows):
             for j in range(0,rows):
                 if i != j:
-                    f_matrix[i][j] = self.f(data[i],data[j])
-        print "f_matrix = ", f_matrix
+                    fuzzy_matrix[i][j] = self.f(self.table[i],self.table[j])
+        temp_fuzzy_matrix = fuzzy_matrix
+        print fuzzy_matrix
         print '\n'
-        return np.multiply(f_matrix,f_matrix)
+        for _ in range(20-1):
+            fuzzy_matrix = np.dot(temp_fuzzy_matrix,fuzzy_matrix)
+        return fuzzy_matrix
 
+    # 根据模糊相似矩阵进行聚类，返回聚类后的矩阵
+    def get_set(self,threshold):
+        fuzzy_matrix = self.get_fuzzy_matrix()
+        assert fuzzy_matrix.shape[0] == fuzzy_matrix.shape[1]
+        r = fuzzy_matrix.shape[0]
+        sets = [] # 分类集合
+        set_matrix = np.dnarray(shape=fuzzy_matrix.shape,dtype=int)
+        
+        for i in range(r):
+            for j in range(r):
+                if i ==j:
+                    set_matrix[i][j] = 1
+                elif fuzzy_matrix[i][j] < threshold:
+                    set_matrix[i][j] = 0
+                else:
+                    fuzzy_matrix[i][j] = 1
+
+        # set_matrix中，值为1说明属于一个集合,否则属于不同集合
+        print "set matrix = ",set_matrix
+        # 把set_matrix看成一个邻接矩阵，DFS就可以得到的联通分量的个数，也就是集合的个数
+        # 以上得到两两的集合，下面合并集合
+        # set_num 集合的个数
+        # sets 聚类后的集合
+        sets = dfs(set_matrix)
+        return sets
 
 if __name__ == '__main__':
     iris = load_iris()
     fdt = FuzzyDecisionTable(iris.data[0:4])
-    print fdt.f(1,0)
-    print fdt.fuzzy_matrix(2)
+    print fdt.get_fuzzy_matrix()
 
