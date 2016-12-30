15a6ec1460a719b201da2bfabb6ea1a39330955b
JerryWang304 <wang_feng_bro@163.com>
2016-12-30 17:57:55 +0800

    correct some mistakes and recode some functions...

--- DFS.py
+++ DFS.py
@@ -3,8 +3,8 @@
 # G: np.ndarray
 import numpy as np
 # 从s出发dfs
-visited = [0 for i in range(2000)]
-def dfs_once(G,s,visited_nodes):
+# visited = [0 for i in range(2000)] 之前把visited定义成全局变量，单个计算每个属性都没问题，但是在run函数中一起计算每个属性就出错
+def dfs_once(G,s,visited_nodes,visited):
     n = G.shape[0] # 顶点个数
     assert G.shape[0] == G.shape[1] and type(s) == int and s >=0 and s < n
     visited[s] = 1 # 当前节点访问结束
@@ -12,21 +12,23 @@ def dfs_once(G,s,visited_nodes):
     #print s
     for i in range(n):
         if G[i][s] == 1 and visited[i] == 0:
-            dfs_once(G,i,visited_nodes)
+            dfs_once(G,i,visited_nodes,visited)
     return
     
 def dfs(G):
     #count = 0
+    visited = [0 for i in range(2000)]
     n = G.shape[0] # 顶点个数
     connected_component = []
     assert G.shape[0] == G.shape[1]
     for i in range(n):
         if visited[i] == 0:
             visited_nodes = []
-            dfs_once(G,i,visited_nodes)
+            dfs_once(G,i,visited_nodes,visited)
             #count += 1
             connected_component.append(visited_nodes)
             #print visited_nodes
+
     return connected_component
 if __name__ == '__main__':
     G = np.array([[1,1,1,0],[1,1,0,0],[1,0,1,0],[0,0,0,1]])
--- fuzzy_decision_table.py
+++ fuzzy_decision_table.py
@@ -1,9 +1,10 @@
-#!/usr/bin/python
+ #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
 # 功能: 生成模糊决策表
 # 作者: 王丰
 # 时间: 2016-12-8
+
 import numpy as np
 from  sklearn.datasets import load_iris
 from math import sqrt
@@ -11,66 +12,86 @@ from DFS import dfs
 class FuzzyDecisionTable(object):
     # table: input data set
     # table type: numpy.ndarray
-    def __init__(self,table):
+    def __init__(self,table,threshold):
         self.table = table
+        self.threshold = threshold
     
-    # 模糊相似关系
-    # input: two real vectors
-    # x,y: ndarry
-    def f(self,x,y):
-        assert x.shape[0] == y.shape[0]
-        nums  = x.shape[0] # 包含元素个数
-        print "x = ",x
-        print "y = ",y
-        print x*y
-        print "sum = ",sum(x*y)
-        numerator = sum(x*y)
-        sum_x = 0 # x 平方和
-        sum_y = 0 # y 平方和 
-        for i in range(0,nums):
-            
-            sum_x += x[i]**2
-            sum_y += y[i]**2
-        length_of_x = sqrt(sum_x)
-        length_of_y = sqrt(sum_y)
-        denomimator = length_of_y*length_of_x
-        return numerator/denomimator
+    # 某个属性下的最大值和最小值
+    def get_max_and_min(self,index):
+        data = self.table[:,index]
+        return max(data),min(data)
+    # 模糊相似关系函数
+    # input: real numbers
+    def f(self,x,y,x_max,x_min):
+        assert x_max > x_min
+        return 1 - abs(x-y)/(x_max - x_min)
 
-    # 用f计算出一个模糊相似矩阵
-    def get_fuzzy_matrix(self):
-        (rows,cols) = self.table.shape
-        fuzzy_matrix = np.identity(rows)
-        # 向量两两计算f
-        for i in range(0,rows):
-            for j in range(0,rows):
-                if i != j:
-                    fuzzy_matrix[i][j] = self.f(self.table[i],self.table[j])
-        temp_fuzzy_matrix = fuzzy_matrix
-        print fuzzy_matrix
-        print '\n'
-        for _ in range(20-1):
-            fuzzy_matrix = np.dot(temp_fuzzy_matrix,fuzzy_matrix)
-        return fuzzy_matrix
 
-    # 根据模糊相似矩阵进行聚类，返回聚类后的矩阵
-    def get_set(self,threshold):
-        fuzzy_matrix = self.get_fuzzy_matrix()
+    # 计算某个属性下的模糊相似矩阵
+    # input: 属性的index
+    def get_fuzzy_matrix(self,index):
+        data = self.table[:,index] # 第index列数据
+        (x_max,x_min) = self.get_max_and_min(index)
+        num = data.shape[0]
+        # 两两计算相似关系
+        # ret: 最后的相似矩阵
+        ret = np.ndarray(shape=(num,num),dtype=float)
+        for i in range(num):
+            for j in range(num):
+                if i == j:
+                    ret[i][j] = 1
+                else:
+                    ret[i][j] = self.f(data[i],data[j],x_max,x_min)
+                    
+        temp = ret
+        #print '相似关系矩阵 = '
+        #print ret
+        #print '\n'
+        # 自乘15次
+        for i in range(8-1): 
+            ret = np.dot(ret,temp)
+        return ret
+
+
+    
+    # def get_fuzzy_matrix(self):
+    #     (rows,cols) = self.table.shape
+    #     fuzzy_matrix = np.identity(rows)
+    #     # 向量两两计算f
+    #     for i in range(0,rows):
+    #         for j in range(0,rows):
+    #             if i != j:
+    #                 fuzzy_matrix[i][j] = self.f(self.table[i],self.table[j])
+    #     temp_fuzzy_matrix = fuzzy_matrix
+    #     print fuzzy_matrix
+    #     print '\n'
+    #     for _ in range(20-1):
+    #         fuzzy_matrix = np.dot(temp_fuzzy_matrix,fuzzy_matrix)
+    #     return fuzzy_matrix
+
+
+    # 根据模糊相似矩阵进行聚类，返回聚类后的集合
+    # 针对的是特定的属性
+    def get_set(self,index):
+        fuzzy_matrix = self.get_fuzzy_matrix(index)
+        #print "fuzzy_matrix %d " % index
+        # print fuzzy_matrix
         assert fuzzy_matrix.shape[0] == fuzzy_matrix.shape[1]
         r = fuzzy_matrix.shape[0]
         sets = [] # 分类集合
-        set_matrix = np.dnarray(shape=fuzzy_matrix.shape,dtype=int)
+        set_matrix = np.ndarray(shape=fuzzy_matrix.shape,dtype=int)
         
         for i in range(r):
             for j in range(r):
                 if i ==j:
                     set_matrix[i][j] = 1
-                elif fuzzy_matrix[i][j] < threshold:
+                elif fuzzy_matrix[i][j] < self.threshold:
                     set_matrix[i][j] = 0
                 else:
-                    fuzzy_matrix[i][j] = 1
+                    set_matrix[i][j] = 1
 
         # set_matrix中，值为1说明属于一个集合,否则属于不同集合
-        print "set matrix = ",set_matrix
+        # print "set matrix = ",set_matrix
         # 把set_matrix看成一个邻接矩阵，DFS就可以得到的联通分量的个数，也就是集合的个数
         # 以上得到两两的集合，下面合并集合
         # set_num 集合的个数
@@ -79,52 +100,87 @@ class FuzzyDecisionTable(object):
         return sets
 
     # some_set: integer set
-    def get_mid_point(self,some_set):
+    # some_set里面存的是数据的下标 （get_set会返回这个）
+    # index是some_set对应的属性下标
+    # return: 返回的也是下标
+    def get_mid_point(self,some_set,index):
+        (x_max,x_min) = self.get_max_and_min(index)
         MIN = 1e9
         n =  len(some_set)
+        assert n > 0
+        if n == 1:
+            return some_set[0]
+
         for i in range(0,n):
-            temp = 0
-            # 计算some_set[i]和剩余的点的模糊相似关系的和
+            sum_i = 0.0
+            # 计算some_set[i]和剩余的数据的模糊相似关系的和
             for j in range(0,n):
                 if i != j:
-                    temp += self.f(self.table[some_set[i]],self.table[some_set[j]])
-                
-            if temp < MIN:
-                MIN = temp
+                    xi = self.table[:,index][some_set[i]]
+                    xj = self.table[:,index][some_set[j]]
+                    # print self.f(xi,xj,x_max,x_min)
+                    sum_i += self.f(xi,xj,x_max,x_min)
+            # print "sum_i = ",sum_i
+            # assert type(sum_i) == float
+            if sum_i < MIN:
+                MIN = sum_i
                 min_point = some_set[i]
+
         return min_point
 
-    # 生成每个集合的中心点
-    def get_mid_points(self):
-        sets = self.get_set()
-        self.sets = sets
+    # 生成某个属性下的中心点
+    def get_mid_points(self, index):
+        sets = self.get_set(index)
+        # print "index = %d" % index
+        # print "sets = ",sets
         n = len(sets) # n 个聚类
         ret = [] # store the indexs of data 
         for s in sets:
-            ret.append(self.get_mid_point(s))
+            ret.append(self.get_mid_point(s,index))
         return ret
-    # 计算每项与聚类的得到的集合的隶属度
-    # 返回类型为[[],[],[]] def li_shu_du(self):
-        mid_points = self.get_mid_points()
-        num_of_sets = len(mid_points) # 集合个数
-        ret = []
-        for i in range(self.table.shape[0]):
-            # i is the index
-            # 分别计算隶属度
-            li_shu_du_temp = []
-            for index in range(num_of_sets):
-                if i == mid_points[index]:
-                    # 若i是中心\
-                    li_shu_du_temp[index] = 1
+
+    # 计算每个属性小的隶属度
+    # 返回类型为[[],[],[],...] 
+    def some_li_shu_du(self, index):
+        mid_points = self.get_mid_points(index)
+        # print "%d ->" % index,
+        # print "mid points = ",mid_points
+        # 聚类的个数n
+        num_set = len(mid_points)
+        (x_max, x_min) = self.get_max_and_min(index)
+        data = self.table[:,index]
+        #print "data = "
+        #print data
+        # 数据的个数
+        num_data = len(data)
+        ret = np.ndarray(shape=(num_data,num_set),dtype=float)
+
+        for i in range(num_data):
+            # data[i]
+            for j in range(num_set):
+                # 如果data[i]是聚类的中心，则为1
+                if i == mid_points[j]:
+                    ret[i][j] = 1
                 else:
-                    li_shu_du_temp[index] = self.f(self.table[mid_points[index]],self.table[i])
-            ret.append(li_shu_du_temp)
+                    ret[i][j] = self.f(data[i], data[mid_points[j]], x_max, x_min)
+                    # print "ret "+str(i)+str(j)+" = ",ret[i][j]
         return ret
 
+    # 返回所有属性的隶属度
+    def run(self):
+        (_,c) = self.table.shape
+        ret = []
 
+        for index in range(c):
+            #print self.some_li_shu_du(index)
+            #print "index = ",index
+            ret.append(self.some_li_shu_du(index))
+
+        return ret 
 
 if __name__ == '__main__':
     iris = load_iris()
-    fdt = FuzzyDecisionTable(iris.data[0:4])
-    print fdt.get_fuzzy_matrix()
+    fdt = FuzzyDecisionTable(iris.data[0:7],threshold=170000)
+    # print fdt.some_li_shu_du(0)
+    print fdt.run()
 
